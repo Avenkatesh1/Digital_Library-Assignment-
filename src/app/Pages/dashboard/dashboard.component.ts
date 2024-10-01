3bdb5fb2fc9de94874bfd2cac77342d860ad90df
A Venkatesh <avenkateshanand1998@gmail.com>
2024-10-02 01:13:08 +0530

    created a charts with the help of chart.js library

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { salesdata } from '../../model/sales';
+import { Chart, registerables } from 'chart.js';
+import { ProductsService } from '../../service/products.service';
+Chart.register(...registerables);
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -7,6 +10,64 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  chartdata: salesdata[] = []
+  labeldata: number[] = [];
+  realdata: number[] = [];
+  colordata: string[] = [];
+
+  productService = inject(ProductsService)
+
+
+  ngOnInit(): void {
+    this.loadchartdata();
+  }
+
+  loadchartdata() {
+    this.productService.loadsalesdata().subscribe(item => {
+      this.chartdata = item;
+      if (this.chartdata != null) {
+        this.chartdata.map(o => {
+          this.labeldata.push(o.year);
+          this.realdata.push(o.amount);
+          this.colordata.push(o.colorcode)
+        })
+        this.Renderbarchart(this.labeldata, this.realdata, this.colordata);
+        this.Renderpiechart(this.labeldata, this.realdata, this.colordata);
+      }
+    });
+  }
+
+  Renderbarchart(labeldata: any, valuedata: any, colordata: any){
+    this.Renderchart(labeldata,valuedata,colordata,'barchart','bar')
+  }
+  Renderpiechart(labeldata: any, valuedata: any, colordata: any){
+    this.Renderchart(labeldata,valuedata,colordata,'piechart','pie')
+  }
+
+  Renderchart(labeldata: any, valuedata: any, colordata: any,chartid:string,charttype:any) {
+    const mychar = new Chart(chartid, {
+      type: charttype,
+      data: {
+        labels: labeldata,
+        datasets: [
+          {
+            label: 'No. Of Students',
+            data: valuedata,
+            backgroundColor: colordata,
+
+          }
+        ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: false
+          }
+        }
+      }
+
+    });
+  }
 }
